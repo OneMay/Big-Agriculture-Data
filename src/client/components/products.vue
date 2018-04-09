f27deb431270369b66d45827b97b2838c19b97df
OneMay <1102304904@qq.com>
2018-04-09 13:37:25 +0800

    jpg

--- src/client/components/products.vue
+++ src/client/components/products.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="pro-jpg">
         <div class="canvas">
             <iframe src="./../../static/canvas.html" :width="width" height="300px"></iframe>
         </div>
@@ -22,205 +22,209 @@
 </template>
 
 <script>
-import changeImg from './changeImg'
-import geo from './geo'
-import detail from './the-product'
-import bmap from './bmap'
-import './../../static/css/lib/bootstrap.css'
-import './../../static/css/lib/component.css'
-//import './../../static/js/lib/modalEffects.js'
-export default {
-    name: 'products',
-    data() {
-        return {
-            width: '100%',
-            productName:'',
-            show:'geo',
-            name:{
-                name:'',
-                map:[]
+    import changeImg from './changeImg'
+    import geo from './geo'
+    import detail from './the-product'
+    import bmap from './bmap'
+    import './../../static/css/lib/bootstrap.css'
+    import './../../static/css/lib/component.css'
+    //import './../../static/js/lib/modalEffects.js'
+    export default {
+        name: 'products',
+        data() {
+            return {
+                width: '100%',
+                productName: '',
+                show: 'geo',
+                name: {
+                    name: '',
+                    map: []
+                },
+                toName: {
+                    name: '',
+                    map: []
+                }
+            }
+        },
+        methods: {
+            getProductName(msg) {
+                this.productName = msg;
+                console.log(this.productName);
             },
-            toName:{
-                name:'',
-                map:[]
-            }
-        }
-    },
-    methods: {
-        getProductName(msg){
-            this.productName=msg;
-            console.log( this.productName);
-        },
-        getToGeo(msg){
-            //alert(msg);
-            //console.log(msg)
-            this.show=msg.show;
-            this.toName.name=msg.name;
-             this.toName.map=msg.arr;
-        },
-        getToBmap(msg){
-            //alert(msg);
-            this.name.name=this.toName.name;
-             this.name.map=this.toName.map;
-            this.toName.name='';
-            this.toName.map=[];
-            this.show=msg.show;
-        },
-        start() {
-            // alert(1)
-            /*!
-             * classie - class helper functions
-             * 
-             * classie.has( elem, 'my-class' ) -> true/false
-             * classie.add( elem, 'my-new-class' )
-             * classie.remove( elem, 'my-unwanted-class' )
-             * classie.toggle( elem, 'my-class' )
-             */
+            getToGeo(msg) {
+                //alert(msg);
+                //console.log(msg)
+                this.show = msg.show;
+                this.toName.name = msg.name;
+                this.toName.map = msg.arr;
+            },
+            getToBmap(msg) {
+                //alert(msg);
+                this.name.name = this.toName.name;
+                this.name.map = this.toName.map;
+                this.toName.name = '';
+                this.toName.map = [];
+                this.show = msg.show;
+            },
+            start() {
+                // alert(1)
+                /*!
+                 * classie - class helper functions
+                 * 
+                 * classie.has( elem, 'my-class' ) -> true/false
+                 * classie.add( elem, 'my-new-class' )
+                 * classie.remove( elem, 'my-unwanted-class' )
+                 * classie.toggle( elem, 'my-class' )
+                 */
 
-            /*jshint browser: true, strict: true, undef: true */
-            /*global define: false */
+                /*jshint browser: true, strict: true, undef: true */
+                /*global define: false */
 
 
-            //'use strict';
+                //'use strict';
 
-            // class helper functions from bonzo https://github.com/ded/bonzo
+                // class helper functions from bonzo https://github.com/ded/bonzo
 
-            function classReg(className) {
-                return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-            }
-
-            // classList support for class management
-            // altho to be fair, the api sucks because it won't accept multiple classes at once
-            var hasClass, addClass, removeClass;
-
-            if ('classList' in document.documentElement) {
-                hasClass = function (elem, c) {
-                    return elem.classList.contains(c);
-                };
-                addClass = function (elem, c) {
-                    elem.classList.add(c);
-                };
-                removeClass = function (elem, c) {
-                    elem.classList.remove(c);
-                };
-            } else {
-                hasClass = function (elem, c) {
-                    return classReg(c).test(elem.className);
-                };
-                addClass = function (elem, c) {
-                    if (!hasClass(elem, c)) {
-                        elem.className = elem.className + ' ' + c;
-                    }
-                };
-                removeClass = function (elem, c) {
-                    elem.className = elem.className.replace(classReg(c), ' ');
-                };
-            }
-
-            function toggleClass(elem, c) {
-                var fn = hasClass(elem, c) ? removeClass : addClass;
-                fn(elem, c);
-            }
-
-            var classie = {
-                // full names
-                hasClass: hasClass,
-                addClass: addClass,
-                removeClass: removeClass,
-                toggleClass: toggleClass,
-                // short names
-                has: hasClass,
-                add: addClass,
-                remove: removeClass,
-                toggle: toggleClass
-            };
-
-            // transport
-            if (typeof define === 'function' && define.amd) {
-                // AMD
-                define(classie);
-            } else {
-                // browser global
-                window.classie = classie;
-            }
-
-            var ModalEffects = (function () {
-
-                function init() {
-
-                    var overlay = document.querySelector('.md-overlay');
-
-                    [].slice.call(document.querySelectorAll('.md-trigger')).forEach(function (el, i) {
-                        //alert(1111);
-                        var modal = document.querySelector('#' + el.getAttribute('data-modal')),
-                            close = modal.querySelector('.md-close');
-
-                        function removeModal(hasPerspective) {
-                            classie.remove(modal, 'md-show');
-
-                            if (hasPerspective) {
-                                classie.remove(document.documentElement, 'md-perspective');
-                            }
-                        }
-
-                        function removeModalHandler() {
-                            removeModal(classie.has(el, 'md-setperspective'));
-                        }
-
-                        el.addEventListener('click', function (ev) {
-                            // alert(ev);
-                            // console.log(ev)
-                            classie.add(modal, 'md-show');
-                            overlay.removeEventListener('click', removeModalHandler);
-                            overlay.addEventListener('click', removeModalHandler);
-
-                            if (classie.has(el, 'md-setperspective')) {
-                                setTimeout(function () {
-                                    classie.add(document.documentElement, 'md-perspective');
-                                }, 25);
-                            }
-                        });
-
-                        close.addEventListener('click', function (ev) {
-                            ev.stopPropagation();
-                            removeModalHandler();
-                        });
-
-                    });
-
+                function classReg(className) {
+                    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
                 }
 
-                init();
+                // classList support for class management
+                // altho to be fair, the api sucks because it won't accept multiple classes at once
+                var hasClass, addClass, removeClass;
 
-            })();
+                if ('classList' in document.documentElement) {
+                    hasClass = function(elem, c) {
+                        return elem.classList.contains(c);
+                    };
+                    addClass = function(elem, c) {
+                        elem.classList.add(c);
+                    };
+                    removeClass = function(elem, c) {
+                        elem.classList.remove(c);
+                    };
+                } else {
+                    hasClass = function(elem, c) {
+                        return classReg(c).test(elem.className);
+                    };
+                    addClass = function(elem, c) {
+                        if (!hasClass(elem, c)) {
+                            elem.className = elem.className + ' ' + c;
+                        }
+                    };
+                    removeClass = function(elem, c) {
+                        elem.className = elem.className.replace(classReg(c), ' ');
+                    };
+                }
+
+                function toggleClass(elem, c) {
+                    var fn = hasClass(elem, c) ? removeClass : addClass;
+                    fn(elem, c);
+                }
+
+                var classie = {
+                    // full names
+                    hasClass: hasClass,
+                    addClass: addClass,
+                    removeClass: removeClass,
+                    toggleClass: toggleClass,
+                    // short names
+                    has: hasClass,
+                    add: addClass,
+                    remove: removeClass,
+                    toggle: toggleClass
+                };
+
+                // transport
+                if (typeof define === 'function' && define.amd) {
+                    // AMD
+                    define(classie);
+                } else {
+                    // browser global
+                    window.classie = classie;
+                }
+
+                var ModalEffects = (function() {
+
+                    function init() {
+
+                        var overlay = document.querySelector('.md-overlay');
+
+                        [].slice.call(document.querySelectorAll('.md-trigger')).forEach(function(el, i) {
+                            //alert(1111);
+                            var modal = document.querySelector('#' + el.getAttribute('data-modal')),
+                                close = modal.querySelector('.md-close');
+
+                            function removeModal(hasPerspective) {
+                                classie.remove(modal, 'md-show');
+
+                                if (hasPerspective) {
+                                    classie.remove(document.documentElement, 'md-perspective');
+                                }
+                            }
+
+                            function removeModalHandler() {
+                                removeModal(classie.has(el, 'md-setperspective'));
+                            }
+
+                            el.addEventListener('click', function(ev) {
+                                // alert(ev);
+                                // console.log(ev)
+                                classie.add(modal, 'md-show');
+                                overlay.removeEventListener('click', removeModalHandler);
+                                overlay.addEventListener('click', removeModalHandler);
+
+                                if (classie.has(el, 'md-setperspective')) {
+                                    setTimeout(function() {
+                                        classie.add(document.documentElement, 'md-perspective');
+                                    }, 25);
+                                }
+                            });
+
+                            close.addEventListener('click', function(ev) {
+                                ev.stopPropagation();
+                                removeModalHandler();
+                            });
+
+                        });
+
+                    }
+
+                    init();
+
+                })();
+            }
+        },
+        mounted() {
+            this.$nextTick(function() {
+                this.start();
+                $('#pro-jpg').removeClass('bodyJpg');
+                $('#pro-jpg').addClass('bodyJpg');
+            })
+        },
+        components: {
+            changeImg,
+            geo,
+            detail,
+            bmap
         }
-    },
-    mounted() {
-        this.$nextTick(function () {
-            this.start();
-        })
-    },
-    components: {
-        changeImg,
-        geo,
-        detail,
-        bmap
-    }
 
-}
+    }
 </script>
 
 <style>
-.canvas {
-    height: 300px;
-}
-#returnGeo{
-    position: relative;
-}
-#click {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-}
+    .canvas {
+        height: 300px;
+    }
+    
+    #returnGeo {
+        position: relative;
+    }
+    
+    #click {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+    }
 </style>
--- src/client/components/transport.vue
+++ src/client/components/transport.vue
@@ -44,224 +44,224 @@
 </template>
 
 <script>
-import changeImg from './changeImg'
-import bmap from './bmap'
-import './../../static/css/lib/bootstrap.css'
-import temperature from './transport/temperature'
-import humidity from './transport/humidity'
-import rain from './transport/rain'
-import echarts from 'echarts'
-import 'echarts/map/js/china.js';
+    import changeImg from './changeImg'
+    import bmap from './bmap'
+    import './../../static/css/lib/bootstrap.css'
+    import temperature from './transport/temperature'
+    import humidity from './transport/humidity'
+    import rain from './transport/rain'
+    import echarts from 'echarts'
+    import 'echarts/map/js/china.js';
 
 
-export default {
-    name: 'database',
-    data() {
-        return {
-            height: 300,
-            Url: './../../static/axios/dataImg.json',
-            width: document.body.clientWidth - 18,
-            setDate: '',
-            teaDate:'',
-            itemDate:{
-                year:'',
-                month:''
+    export default {
+        name: 'database',
+        data() {
+            return {
+                height: 300,
+                Url: './../../static/axios/dataImg.json',
+                width: document.body.clientWidth - 18,
+                setDate: '',
+                teaDate: '',
+                itemDate: {
+                    year: '',
+                    month: ''
+                },
+                envirDate: {
+                    year: '',
+                    month: '',
+                    day: '',
+                    num: null,
+                    hours: ''
+                },
+                classObj: {
+                    'btn-danger': false,
+                    'btn-success': true
+                },
+                classObj2: {
+                    'btn-danger': true,
+                    'btn-success': true
+                },
+                classObj3: {
+                    'btn-danger': true,
+                    'btn-success': true
+                },
+                show: 'temperature',
+                getDOM: null,
+            }
+        },
+        methods: {
+            setItDay(num) {
+                var getDom = document.getElementById('temperatureDom').value;
+                var reg = /(\d{4})-(.{2})-(.+)/g;
+                var partDate = getDom.split(reg);
+                this.envirDate.year = partDate[1];
+                this.envirDate.month = partDate[2];
+                this.envirDate.day = partDate[3];
+                this.envirDate.num = num;
             },
-            envirDate:{
-                year:'',
-                month:'',
-                day:'',
-                num:null,
-                hours:''
+            chartOne(index) {
+                this.setDay(0);
+                if (index == 1) {
+                    this.show = 'temperature';
+                    this.classObj['btn-danger'] = false;
+                    this.classObj2['btn-danger'] = true;
+                    this.classObj3['btn-danger'] = true;
+                }
+                if (index == 2) {
+                    this.show = 'humidity';
+                    this.classObj['btn-danger'] = true;
+                    this.classObj2['btn-danger'] = false;
+                    this.classObj3['btn-danger'] = true;
+                }
+                if (index == 3) {
+                    this.show = 'rain';
+                    this.classObj['btn-danger'] = true;
+                    this.classObj2['btn-danger'] = true;
+                    this.classObj3['btn-danger'] = false;
+                }
             },
-            classObj: {
-                'btn-danger': false,
-                'btn-success': true
+            setDay(num) {
+                var date = new Date();
+                var year = date.getFullYear();
+                var month = date.getMonth() + 1;
+                var day = date.getDate();
+                var hour = date.getHours();
+                var teamonth = month - 1;
+                if (month < 10) {
+                    month = '0' + month;
+                }
+                if (teamonth == 0) {
+                    teamonth = 12;
+                    year = year - 1;
+                }
+                if (teamonth < 10) {
+                    teamonth = '0' + teamonth;
+                }
+                if (day < 10) {
+                    day = '0' + day;
+                }
+                var dateString = date.getFullYear() + "-" + month + "-" + day;
+                var teaDay = date.getFullYear() + "-" + teamonth;
+                var strTMonth = teamonth.toString();
+                var strEMonth = month.toString();
+                this.setDate = dateString;
+                //this.teaDate=teaDay;
+                //this.itemDate.year=year.toString();
+                // this.itemDate.month=strTMonth;
+                this.envirDate.year = year.toString();
+                this.envirDate.month = strEMonth;
+                this.envirDate.day = day.toString();
+                this.envirDate.num = num;
+                this.envirDate.hours = hour;
             },
-            classObj2: {
-                'btn-danger': true,
-                'btn-success': true
-            },
-            classObj3: {
-                'btn-danger': true,
-                'btn-success': true
-            },
-            show: 'temperature',
-            getDOM:null,
+            getDates() {
+                //alert(window.location.hash);
+                var date = new Date();
+                var year = date.getFullYear();
+                var month = date.getMonth() + 1;
+                var day = date.getDate();
+                var hour = date.getHours();
+                var teamonth = month - 1;
+                if (month < 10) {
+                    month = '0' + month;
+                }
+                if (teamonth == 0) {
+                    teamonth = 12;
+                    year = year - 1;
+                }
+                if (teamonth < 10) {
+                    teamonth = '0' + teamonth;
+                }
+                if (day < 10) {
+                    day = '0' + day;
+                }
+                var dateString = date.getFullYear() + "-" + month + "-" + day;
+                var teaDay = date.getFullYear() + "-" + teamonth;
+                var strTMonth = teamonth.toString();
+                var strEMonth = month.toString();
+                this.setDate = dateString;
+                this.teaDate = teaDay;
+                this.itemDate.year = year.toString();
+                this.itemDate.month = strTMonth;
+                this.envirDate.year = year.toString();
+                this.envirDate.month = strEMonth;
+                this.envirDate.day = day.toString();
+                this.envirDate.num = 0;
+                this.envirDate.hours = hour;
+            }
+        },
+        mounted() {
+            this.$nextTick(function() {
+                this.getDates();
+
+            })
+        },
+        components: {
+            changeImg,
+            temperature,
+            humidity,
+            rain,
+            bmap
         }
-    },
-    methods: {
-        setItDay(num){
-            var getDom = document.getElementById('temperatureDom').value;
-            var reg = /(\d{4})-(.{2})-(.+)/g;
-            var partDate = getDom.split(reg);
-            this.envirDate.year=partDate[1];
-            this.envirDate.month=partDate[2];
-            this.envirDate.day=partDate[3];
-            this.envirDate.num=num;
-        },
-        chartOne(index) {
-            this.setDay(0);
-            if (index == 1) {
-                this.show = 'temperature';
-                this.classObj['btn-danger'] = false;
-                this.classObj2['btn-danger'] = true;
-                this.classObj3['btn-danger'] = true;
-            }
-            if (index == 2) {
-                this.show = 'humidity';
-                this.classObj['btn-danger'] = true;
-                this.classObj2['btn-danger'] = false;
-                this.classObj3['btn-danger'] = true;
-            }
-            if (index == 3) {
-                this.show = 'rain';
-                this.classObj['btn-danger'] = true;
-                this.classObj2['btn-danger'] = true;
-                this.classObj3['btn-danger'] = false;
-            }
-        },
-        setDay(num) {
-            var date = new Date();
-            var year=date.getFullYear();
-            var month = date.getMonth() + 1;
-            var day = date.getDate();
-            var hour = date.getHours();
-            var teamonth=month-1;
-            if (month < 10) {
-                month = '0' + month;
-            }
-            if(teamonth==0){
-                teamonth=12;
-                year=year-1;
-            }
-            if (teamonth < 10) {
-                teamonth = '0' + teamonth;
-            }
-            if (day < 10) {
-                day = '0' + day;
-            }
-            var dateString = date.getFullYear() + "-" + month + "-" + day;
-            var teaDay=date.getFullYear() + "-" + teamonth;
-            var strTMonth=teamonth.toString();
-            var strEMonth=month.toString();
-            this.setDate = dateString;
-            //this.teaDate=teaDay;
-            //this.itemDate.year=year.toString();
-           // this.itemDate.month=strTMonth;
-            this.envirDate.year=year.toString();
-            this.envirDate.month=strEMonth;
-            this.envirDate.day=day.toString();
-            this.envirDate.num=num;
-            this.envirDate.hours=hour;
-        },
-        getDates() {
-            //alert(window.location.hash);
-            var date = new Date();
-            var year=date.getFullYear();
-            var month = date.getMonth() + 1;
-            var day = date.getDate();
-            var hour=date.getHours();
-            var teamonth=month-1;
-            if (month < 10) {
-                month = '0' + month;
-            }
-            if(teamonth==0){
-                teamonth=12;
-                year=year-1;
-            }
-            if (teamonth < 10) {
-                teamonth = '0' + teamonth;
-            }
-            if (day < 10) {
-                day = '0' + day;
-            }
-            var dateString = date.getFullYear() + "-" + month + "-" + day;
-            var teaDay=date.getFullYear() + "-" + teamonth;
-            var strTMonth=teamonth.toString();
-            var strEMonth=month.toString();
-            this.setDate = dateString;
-            this.teaDate=teaDay;
-            this.itemDate.year=year.toString();
-            this.itemDate.month=strTMonth;
-            this.envirDate.year=year.toString();
-            this.envirDate.month=strEMonth;
-            this.envirDate.day=day.toString();
-            this.envirDate.num=0;
-            this.envirDate.hours=hour;
-        }
-    },
-    mounted() {
-        this.$nextTick(function () {
-            this.getDates();
-        })
-    },
-    components: {
-        changeImg,
-        temperature,
-        humidity,
-        rain,
-        bmap
+
     }
-
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/** 可以设置不同的进入和离开动画 */
-
-
-/* 设置持续时间和动画函数 */
-
-.bounce-enter-active {
-    animation: bounce-in .5s;
-}
-
-.bounce-leave-active {
-    animation: bounce-in .5s reverse;
-}
-
-@keyframes bounce-in {
-    0% {
-        transform: scale(0);
+    /** 可以设置不同的进入和离开动画 */
+    /* 设置持续时间和动画函数 */
+    
+    .bounce-enter-active {
+        animation: bounce-in .5s;
     }
-    50% {
-        transform: scale(1.5);
+    
+    .bounce-leave-active {
+        animation: bounce-in .5s reverse;
     }
-    100% {
-        transform: scale(1);
+    
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
     }
-}
-
-.environment {
-    clear: both;
-}
-
-.form-horizontal.horizon1 {
-    margin-top: 0.8em;
-    margin-left: 20em;
-}
-
-.horizon3 {
-    margin-top: -1.5rem;
-}
-
-button.btn.btn-danger.btn-xs.horizon2 {
-    margin-top: 5px;
-}
-
-.charts {
-    border: 1px solid #ddd;
-}
-
-.environment {
-    margin: 20px auto;
-}
-.tea-Data{
-    text-align:center;
-    border-bottom:1px solid #ddd;
-    margin: 0;
-    padding:10px;
-}
+    
+    .environment {
+        clear: both;
+    }
+    
+    .form-horizontal.horizon1 {
+        margin-top: 0.8em;
+        margin-left: 20em;
+    }
+    
+    .horizon3 {
+        margin-top: -1.5rem;
+    }
+    
+    button.btn.btn-danger.btn-xs.horizon2 {
+        margin-top: 5px;
+    }
+    
+    .charts {
+        border: 1px solid #ddd;
+    }
+    
+    .environment {
+        margin: 20px auto;
+    }
+    
+    .tea-Data {
+        text-align: center;
+        border-bottom: 1px solid #ddd;
+        margin: 0;
+        padding: 10px;
+    }
 </style>
